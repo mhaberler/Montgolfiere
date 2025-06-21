5a68b2a5fd06b8abcddb7673fc524b44b2accd99
Michael Haberler <git@mah.priv.at>
2025-06-21 14:54:26 +0200

    add command line build, debug upload for Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,32 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 const config: CapacitorConfig = {
   appId: 'com.haberler.sensordemo',
   appName: 'sensordemo',
-  webDir: 'dist'
+  webDir: 'dist',
+
+
+  android: {
+    buildOptions: {
+      keystorePath: process.env.ANDROID_KEYSTORE_PATH || './debug.keystore', // Fallback to a debug keystore for development
+
+      // Keystore Password
+      // Read from process.env.ANDROID_KEYSTORE_PASSWORD
+      keystorePassword: process.env.ANDROID_KEYSTORE_PASSWORD || 'android', // Fallback for debug
+
+      // Keystore Alias
+      // Read from process.env.ANDROID_KEYSTORE_ALIAS
+      keystoreAlias: process.env.ANDROID_KEY_ALIAS || 'androiddebugkey', // Fallback for debug
+
+      // Keystore Alias Password
+      // Read from process.env.ANDROID_KEYSTORE_ALIAS_PASSWORD
+      keystoreAliasPassword: process.env.ANDROID_KEYSTORE_ALIAS_PASSWORD || 'android', // Fallback for debug
+    },
+    webContentsDebuggingEnabled: true
+  }
 };
 
 export default config;
