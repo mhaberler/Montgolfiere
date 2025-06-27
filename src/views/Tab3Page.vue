5a621d73b8e698b59306d9ce3398985007da5719
Michael Haberler <git@mah.priv.at>
2025-06-27 23:58:31 +0200

    cleanup tab bar, Deebug page

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -1,10 +1,10 @@
 <template>
   <ion-page>
-    <!-- <ion-header>
+    <ion-header>
       <ion-toolbar>
         <ion-title>Status</ion-title>
       </ion-toolbar>
-    </ion-header> -->
+    </ion-header>
     <ion-content :fullscreen="true">
       <DebugEkf v-if="showDebugInfo"></DebugEkf>
     </ion-content>
@@ -39,51 +39,29 @@ import {
   IonCardContent,
   IonContent,
   // IonText,
-  IonCol, IonGrid, IonRow
+  // IonCol, IonGrid, IonRow
 } from '@ionic/vue';
 
 
 import {
-  locationAvailable, location, locationError,
-  referencePressure,
-  useReferencePressure,
-  pressure,
-  altitudeQNH,
-  altitudeISA,
-  barometerAvailable,
-  ekfAltitude,
-  ekfVelocity,
-  ekfAcceleration,
-  ekfBurnerGain,
-  ekfIsDecelerating,
-  ekfTimeToZeroSpeed,
-  ekfZeroSpeedAltitude,
-  ekfZeroSpeedValid
+  // locationAvailable, location, locationError,
+  // referencePressure,
+  // useReferencePressure,
+  // pressure,
+  // altitudeQNH,
+  // altitudeISA,
+  // barometerAvailable,
+  // ekfAltitude,
+  // ekfVelocity,
+  // ekfAcceleration,
+  // ekfBurnerGain,
+  // ekfIsDecelerating,
+  // ekfTimeToZeroSpeed,
+  // ekfZeroSpeedAltitude,
+  // ekfZeroSpeedValid,
+  showDebugInfo
 
 } from '../utils/state';
 
-const showDebugInfo = import.meta.env.MODE === 'development';
 
-// const handleSensorClick = (sensorData) => {
-//   console.log('Sensor clicked:', sensorData)
-// }
 </script>
-
-<style scoped>
-.sensor-grid {
-  /* gap: 5px; */
-}
-
-.sensor-col {
-  padding: 0px;
-}
-
-.sensor-container {
-  /* padding: 0px; */
-  display: flex;
-  flex-direction: column;
-  /* align-items: center; */
-  gap: 0px;
-  margin-top: 1px
-}
-</style>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,22 +4,22 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <!-- <ion-icon aria-hidden="true" :icon="triangle" /> -->
           <ion-label>Status</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+          <!-- <ion-icon aria-hidden="true" :icon="ellipse" /> -->
           <ion-label>Sensors</ion-label>
         </ion-tab-button>
 
         <ion-tab-button v-if="showDebugInfo" tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <!-- <ion-icon aria-hidden="true" :icon="square" /> -->
           <ion-label>Debug</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="settings" href="/tabs/settings">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+          <!-- <ion-icon aria-hidden="true" :icon="ellipse" /> -->
           <ion-label>Settings</ion-label>
         </ion-tab-button>
         
