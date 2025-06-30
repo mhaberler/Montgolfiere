1f7c43caee4633f4eed74940d071918541e623b5
Michael Haberler <git@mah.priv.at>
2025-06-30 07:30:24 +0200

    @capawesome/capacitor-screen-orientation cfg

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,14 +1,14 @@
 import UIKit
 import Capacitor
+import CapawesomeCapacitorScreenOrientation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return ScreenOrientation.getSupportedInterfaceOrientations()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
