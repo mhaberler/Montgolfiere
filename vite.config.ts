c1202f1e5bc24df3e0364cf1be0a303133d632b3
Michael Haberler <git@mah.priv.at>
2025-07-02 11:27:40 +0200

    version 1.5.6

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import { qrcode } from "vite-plugin-qrcode";
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const branchName = execSync("git rev-parse --abbrev-ref HEAD")
+  .toString()
+  .trim();
+const buildDate = execSync("date -u +%Y-%m-%dT%H:%M:%SZ").toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
@@ -36,6 +46,10 @@ export default defineConfig(({ mode, command }) => {
     define: {
       // demo variable
       __API_URL__: JSON.stringify(env.VITE_API_URL),
+      __GIT_COMMIT_HASH__: JSON.stringify(commitHash),
+      __GIT_BRANCH_NAME__: JSON.stringify(branchName),
+      __VITE_BUILD_DATE__: JSON.stringify(buildDate),
+      __APP_VERSION__: JSON.stringify(packageJson.version),
     },
   };
 });
