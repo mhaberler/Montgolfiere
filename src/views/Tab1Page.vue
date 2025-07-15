16b125089ca6ae4dcefe7619abc1aede3fcd64f3
Michael Haberler <git@mah.priv.at>
2025-07-15 20:48:16 +0200

    style nits

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -44,21 +44,19 @@
                         <!-- <div> -->
 
                         <LinearScale :value="ekfVelocity" :orientation="'vertical'" :scalePadding="15"
-                            :percentCenter="50" :percentMid="30" :percentOuter="20" :indicatorSize="20"
-                            :confidenceRangePercent="10" :confidenceBoxCrossDimension="20" :transitionDuration="0.95"
-                            :majorTicks="vsiMajorTicks" :minorTicks="vsiMinorTicks"
+                            :indicatorSize="20" :confidenceRangePercent="10" :confidenceBoxCrossDimension="20"
+                            :transitionDuration="0.95" :majorTicks="vsiMajorTicks" :minorTicks="vsiMinorTicks"
                             :intermediateTicks="vsiIntermediateTicks" :weights="vsiWeights"
-                            :majorTickTextOffset="vsiMajorTickTextOffset" :indicatorDistancePercent="12" />
+                            :majorTickTextOffset="vsiMajorTickTextOffset" :indicatorDistancePercent="22" />
                         <!-- </div> -->
                     </div>
 
                     <div class="row-span-3 col-span-1">
                         <LinearScale :value="ekfAcceleration" :orientation="'vertical'" :scalePadding="15"
-                            :percentCenter="50" :percentMid="30" :percentOuter="20" :indicatorSize="20"
-                            :confidenceRangePercent="10" :confidenceBoxCrossDimension="20" :transitionDuration="0.95"
-                            :majorTicks="vaccMajorTicks" :minorTicks="vaccMinorTicks"
+                            :indicatorSize="20" :confidenceRangePercent="10" :confidenceBoxCrossDimension="20"
+                            :transitionDuration="0.95" :majorTicks="vaccMajorTicks" :minorTicks="vaccMinorTicks"
                             :intermediateTicks="vaccIntermediateTicks" :weights="vaccWeights"
-                            :majorTickTextOffset="vaccMajorTickTextOffset" :indicatorDistancePercent="12" />
+                            :majorTickTextOffset="vaccMajorTickTextOffset" :indicatorDistancePercent="22" />
                     </div>
 
 
@@ -117,19 +115,19 @@ import LinearScale from '../components/LinearScale.vue';
 
 const vsiMajorTicks = ref([-10, -5, -1, 0, 1, 5, 10]);
 // const vsiMinorTicks = ref([-9, -8, -7, -6, -4, -3, -2, -1, 1, 2, 3, 4, 6, 7, 8, 9,]);
-const vsiMinorTicks = ref([-0.9,-0.8,-0.7,-0.6,-0.4,-0.3,-0.2, -0.1,
-    0.1,0.2,0.3, 0.4, 0.6,0.7,0.8,0.9
+const vsiMinorTicks = ref([-0.9, -0.8, -0.7, -0.6, -0.4, -0.3, -0.2, -0.1,
+    0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9
 ]);
-const vsiIntermediateTicks = ref([-9,-8,-7,-6,-4,-3,-2,-0.5, 0.5,2,3,4,6,7,8,9]);
+const vsiIntermediateTicks = ref([-9, -8, -7, -6, -4, -3, -2, -0.5, 0.5, 2, 3, 4, 6, 7, 8, 9]);
 const vsiWeights = ref([0.1, 0.15, 0.25, 0.25, 0.15, 0.1]); // Must sum to ~1.0 and match segments
 const vsiMajorTickTextOffset = ref(5)
 
 
 
 const vaccMajorTicks = ref([-1.0, -0.5, -0.1, 0, 0.1, 0.5, 1.0]);
-const vaccMinorTicks = ref([-0.09,-0.08,-0.07,-0.06,-0.04,-0.03,-0.02,-0.01,0.01,0.02,0.03,0.04,0.06,0.07,0.08,0.09
+const vaccMinorTicks = ref([-0.09, -0.08, -0.07, -0.06, -0.04, -0.03, -0.02, -0.01, 0.01, 0.02, 0.03, 0.04, 0.06, 0.07, 0.08, 0.09
 ]);
-const vaccIntermediateTicks = ref([-0.9,-0.8,-0.7,-0.6,-0.4,-0.3,-0.2,-0.1,-0.05, 0.05,0.2,0.3,0.4,0.6,0.7,0.8,0.9]);
+const vaccIntermediateTicks = ref([-0.9, -0.8, -0.7, -0.6, -0.4, -0.3, -0.2, -0.1, -0.05, 0.05, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9]);
 const vaccWeights = ref([0.1, 0.15, 0.25, 0.25, 0.15, 0.1]); // Must sum to ~1.0 and match segments
 const vaccMajorTickTextOffset = ref(5)
 
