86683d384d5eba19a536d542cadb740b354e2103
Michael Haberler <git@mah.priv.at>
2025-07-16 21:48:18 +0200

    clean up

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -6,29 +6,21 @@
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
-
-
             <div class="units-container">
                 <div class="units-grid">
-                    <LocationDisplay></LocationDisplay>
-
                     <ValueCard :value="clicks" :name="vcname" :unit="einheit" :tick="ticker" :batteryStatus="32" />
                     <div>
                         <button @click="incrementValue">Increment</button>
                     </div>
-                    <!-- <EKFDisplay>
-              </EKFDisplay>
-              <EnvelopeUnit @assign-device="goToDeviceAssignment" />
-              <OATUnit @assign-device="goToDeviceAssignment" />
-              <TankUnit unit-type="Tank1" @assign-device="goToDeviceAssignment" />
-              <TankUnit unit-type="Tank2" @assign-device="goToDeviceAssignment" />
-              <TankUnit unit-type="Tank3" @assign-device="goToDeviceAssignment" />
-              <BoxUnit unit-type="Box" @assign-device="goToDeviceAssignment" /> -->
+                    <EnvelopeUnit @assign-device="goToDeviceAssignment" />
+                    <OATUnit @assign-device="goToDeviceAssignment" />
+                    <TankUnit unit-type="Tank1" @assign-device="goToDeviceAssignment" />
+                    <TankUnit unit-type="Tank2" @assign-device="goToDeviceAssignment" />
+                    <TankUnit unit-type="Tank3" @assign-device="goToDeviceAssignment" />
+                    <BoxUnit unit-type="Box" @assign-device="goToDeviceAssignment" /> -->
                 </div>
             </div>
         </ion-content>
-
-
     </ion-page>
 </template>
 
@@ -40,17 +32,9 @@ import {
     IonTitle,
     IonContent
 } from '@ionic/vue';
+
 import { useRouter } from 'vue-router';
 import { ticker } from '../utils/state';
-
-
-import EnvelopeUnit from '@/components/units/EnvelopeUnit.vue';
-import OATUnit from '@/components/units/OATUnit.vue';
-import TankUnit from '@/components/units/TankUnit.vue';
-import BoxUnit from '@/components/units/BoxUnit.vue';
-import DebugEkf from '@/components/DebugEkf.vue';
-import LocationDisplay from '@/components/LocationDisplay.vue';
-import EKFDisplay from '@/components/EKFDisplay.vue';
 import ValueCard from '@/components/ValueCard.vue';
 
 const router = useRouter();
@@ -90,4 +74,3 @@ function incrementValue() {
     }
 }
 </style>
-  
