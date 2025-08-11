129826cbfa825dcf06c17e4a2142a7e0afe914f6
Michael Haberler <git@mah.priv.at>
2025-08-11 20:21:49 +0200

    auto-hide navbar on inactivity

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-tabs class="pb-8">
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar slot="bottom" id="tab-bar">
 
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="airplaneOutline" />
@@ -14,13 +14,12 @@
           <ion-label>Sensors</ion-label>
         </ion-tab-button>
 
-
-        <ion-tab-button v-if="showDebugInfo"  tab="mdns" href="/tabs/mdns">
+        <ion-tab-button v-if="showDebugInfo" tab="mdns" href="/tabs/mdns">
           <ion-icon aria-hidden="true" :icon="flashlightOutline" />
           <ion-label>Scan</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button v-if="showDebugInfo"  tab="mqtt" href="/tabs/mqtt">
+        <ion-tab-button v-if="showDebugInfo" tab="mqtt" href="/tabs/mqtt">
           <ion-icon aria-hidden="true" :icon="flashOutline" />
           <ion-label>MQTT</ion-label>
         </ion-tab-button>
@@ -43,7 +42,51 @@
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { airplaneOutline, bluetoothOutline, cloudDownloadOutline, settingsOutline, flashOutline, flashlightOutline } from 'ionicons/icons';
-import {
-  showDebugInfo,
-} from '@/utils/state';
+import { showDebugInfo } from '@/utils/state';
+import { onMounted, onUnmounted } from 'vue';
+import { StatusBar, Style } from '@capacitor/status-bar';
+
+const INACTIVITY_TIME = 5 * 1000; // seconds
+let timeoutId: ReturnType<typeof setTimeout> | undefined;
+
+const resetTimer = () => {
+  clearTimeout(timeoutId);
+  showTabs();
+  timeoutId = setTimeout(hideTabs, INACTIVITY_TIME);
+};
+
+const hideTabs = () => {
+  const tabBar = document.querySelector('#tab-bar') as HTMLElement;
+  if (tabBar) tabBar.style.display = 'none';
+  // StatusBar.hide();
+
+};
+
+const showTabs = () => {
+  const tabBar = document.querySelector('#tab-bar') as HTMLElement;
+  if (tabBar) tabBar.style.display = 'flex';
+  // StatusBar.show();
+};
+
+const setupActivityListeners = () => {
+  ['click', 'mousemove', 'keypress', 'scroll'].forEach(event => {
+    document.addEventListener(event, resetTimer);
+  });
+};
+
+const removeActivityListeners = () => {
+  ['click', 'mousemove', 'keypress', 'scroll'].forEach(event => {
+    document.removeEventListener(event, resetTimer);
+  });
+};
+
+onMounted(() => {
+  resetTimer();
+  setupActivityListeners();
+});
+
+onUnmounted(() => {
+  clearTimeout(timeoutId);
+  removeActivityListeners();
+});
 </script>
