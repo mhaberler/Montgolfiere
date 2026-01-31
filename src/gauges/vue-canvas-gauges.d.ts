5ac0d639874b9315d5f846271df7e5391d95c21c
Michael Haberler <git@mah.priv.at>
2026-01-31 06:25:55 +0100

    remove unused gauges

--- src/gauges/vue-canvas-gauges.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@/gauges/vue-canvas-gauges' {
-  import LinearGauge from '@/gauges/LinearGauge.vue';
-  import RadialGauge from '@/gauges/RadialGauge.vue';
-  export { LinearGauge, RadialGauge };
-}
--- src/gauges/vue-canvas-gauges.js
+++
@@ -1,7 +0,0 @@
-  import LinearGauge from '@/gauges/LinearGauge.vue';
-  import RadialGauge from '@/gauges/RadialGauge.vue';
-
-export {
-  LinearGauge,
-  RadialGauge
-}
